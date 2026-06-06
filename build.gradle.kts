60b2a0464c569ef8f070d169da9982a4284eb260
Rafael Ribeiro <work.rafaelribeiro@gmail.com>
2026-06-06 12:41:21 +0100

    chore: add template (#1)

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
+    alias(libs.plugins.android.kmp.library).apply(false)
+    alias(libs.plugins.maven.publish).apply(false)
+    alias(libs.plugins.kotlinx.serialization).apply(false)
+}
