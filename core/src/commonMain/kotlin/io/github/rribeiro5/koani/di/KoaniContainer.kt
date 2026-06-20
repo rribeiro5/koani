93083a2165b798e5b6e5e64ae931f18244507461
Rafael Ribeiro <work.rafaelribeiro@gmail.com>
2026-06-20 12:47:35 +0100

    chore: Configure Logger and KtorLogger

--- core/src/commonMain/kotlin/io/github/rribeiro5/koani/di/KoaniContainer.kt
+++ core/src/commonMain/kotlin/io/github/rribeiro5/koani/di/KoaniContainer.kt
@@ -1,24 +1,51 @@
 package io.github.rribeiro5.koani.di
 
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.Severity
+import co.touchlab.kermit.loggerConfigInit
+import co.touchlab.kermit.platformLogWriter
+import io.github.rribeiro5.koani.LogLevel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import io.ktor.client.plugins.logging.LogLevel as KtorLogLevel
+import io.ktor.client.plugins.logging.Logger as KtorLogger
 
 internal class KoaniContainer(
     clientId: String,
     timeoutMillis: Long? = null,
+    logLevel: LogLevel = LogLevel.NONE,
     engine: HttpClientEngine = getEngine(),
 ) {
 
     companion object {
+        private const val LOGGER_TAG = "KoaniClient"
+
         private const val BASE_URL = "https://api.myanimelist.net/"
         private const val CLIENT_ID_HEADER = "X-MAL-CLIENT-ID"
     }
+
+    val logger = Logger(
+        config = loggerConfigInit(
+            platformLogWriter(),
+            minSeverity = when (logLevel) {
+                LogLevel.VERBOSE -> Severity.Verbose
+                LogLevel.DEBUG -> Severity.Debug
+                LogLevel.INFO -> Severity.Info
+                LogLevel.WARN -> Severity.Warn
+                LogLevel.ERROR -> Severity.Error
+                LogLevel.ASSERT -> Severity.Assert
+                LogLevel.NONE -> Severity.Assert
+            }
+        ),
+        tag = LOGGER_TAG
+    )
 
     private val httpClient: HttpClient = HttpClient(engine) {
         expectSuccess = true
@@ -36,6 +63,17 @@ internal class KoaniContainer(
         }
         install(HttpTimeout) {
             requestTimeoutMillis = timeoutMillis
+        }
+
+        if (logLevel != LogLevel.NONE) {
+            install(Logging) {
+                logger = object : KtorLogger {
+                    override fun log(message: String) {
+                        this@KoaniContainer.logger.v { message }
+                    }
+                }
+                level = KtorLogLevel.ALL
+            }
         }
     }
 }
