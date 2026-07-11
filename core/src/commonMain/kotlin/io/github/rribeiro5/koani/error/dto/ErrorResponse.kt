a0159f88db43f7296dafa4936c2a484104c4ea42
Rafael Ribeiro <work.rafaelribeiro@gmail.com>
2026-07-11 15:37:13 +0100

    feat: renamed ErrorDto to ErrorResponse

--- core/src/commonMain/kotlin/io/github/rribeiro5/koani/error/dto/ErrorResponse.kt
+++ core/src/commonMain/kotlin/io/github/rribeiro5/koani/error/dto/ErrorResponse.kt
@@ -1,9 +1,9 @@
-package io.github.rribeiro5.koani.error
+package io.github.rribeiro5.koani.error.dto
 
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class ErrorDto(
+internal data class ErrorResponse(
     val error: String? = null,
     val message: String? = null
 )
